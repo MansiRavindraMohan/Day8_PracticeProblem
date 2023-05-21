286beffd5e6e241c6a85ba7f8247bbc096a58962
MansiRavindraMohan <manasimohan29@gmail.com>
2023-05-22 00:00:11 +0530

    Add: Program for checking string is matching with given pattern or not

---
+++ Day8_PracticeProblem/MatchingString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day8_PracticeProblem
+{
+    public class MatchingString
+    {
+        public void MethodString()
+        {
+            string input1 = "abb";      // Match
+            string input2 = "abbb";     // Match
+            string input3 = "abtfui";    // No match
+            string input4 = "acbb";     // No match
+
+            string pattern = @"ab{2,3}";
+
+            Console.WriteLine("Matching pattern: " + pattern);
+            Console.WriteLine();
+
+            MatchString(input1, pattern);
+            MatchString(input2, pattern);
+            MatchString(input3, pattern);
+            MatchString(input4, pattern);
+        }
+
+        public static void MatchString(string input, string pattern)
+        {
+            bool isMatch = Regex.IsMatch(input, pattern);
+
+            Console.WriteLine("Input: " + input);
+            Console.WriteLine("Match: " + isMatch);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Day8_PracticeProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_PracticeProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MatchingString matchingString = new MatchingString();
+            matchingString.MethodString();
+            Console.ReadLine();
+        }
+    }
+}
